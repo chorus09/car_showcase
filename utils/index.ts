98a82a9b8284f50a12bf21934b035a2b667604e3
chorus09 <thetrumer@gmail.com>
2025-05-22 00:44:07 +0300

    utils and rapidapi

---
+++ utils/index.ts
@@ -0,0 +1,36 @@
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '',
+		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+
+export async function fetchCars() {
+
+    const headers = {
+    'X-RapidAPI-Key': '',
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+
+    const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars}`,
+    {
+      headers: headers,
+    }
+  );
+
+    const result = await response.json();
+
+    return result;
+}
