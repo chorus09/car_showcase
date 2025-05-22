41da44264c410697ff730e3aada5a8e41b774184
chorus09 <thetrumer@gmail.com>
2025-05-22 23:06:59 +0300

    car images api

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProperties } from "@/types";
+
 export async function fetchCars() {
 
     const headers = {
@@ -32,3 +34,19 @@ export const calculateCarRent = (year: number, cylinders: number, displacement: 
 
   return rentalRatePerDay.toFixed(0);
 };
+
+export const generateCarImageUrl = (car: CarProperties, angle? : string) => {
+    const url = new URL('https://cdn.imagin.studio/getimage');
+
+    const { make, year, model } = car;
+    
+    url.searchParams.append('customer', 'hrjavascript-mastery');
+
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(' ')[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    url.searchParams.append('angle', `${angle}`);
+
+    return `${url}`;
+}
