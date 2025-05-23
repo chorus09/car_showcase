7c3377b168d29be044541051c0f084e1a8d7c98d
chorus09 <thetrumer@gmail.com>
2025-05-23 03:45:01 +0300

    filters is done (with change search)

--- utils/index.ts
+++ utils/index.ts
@@ -52,3 +52,11 @@ export const generateCarImageUrl = (car: CarProperties, angle?: string) => {
 
   return `${url}`;
 };
+
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  searchParams.set(type, value);
+
+  return `${window.location.pathname}?${searchParams.toString()}`;
+}
